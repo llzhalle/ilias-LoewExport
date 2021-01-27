c4679c495f2e4a8bc8766be7f3fce2da23296cfd
nilsfischer-llz <58814282+nilsfischer-llz@users.noreply.github.com>
2021-01-27 12:08:26 +0100

    Fix and add Support IMG for Tooltip

--- templates/loepoexp.js
+++ templates/loepoexp.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {	
 	var l = $('html').attr('lang') || 'en';
 
-	var elem_lpdate = '<div class="navbar-form"><label for="lpnum">'+loepoexp_lang.label_date[l]+'<span class="glyphicon glyphicon-info-sign" data-image="löwenportal.png"></span>:</label> <select class="form-control" id="lpdate" name="lpdate" style="width:40px;padding:0;">'+
+	var elem_lpdate = '<div class="navbar-form"><label for="lpdate">'+loepoexp_lang.label_date[l]+'<span id="lpdate_support" class="glyphicon glyphicon-info-sign" data-image="lpdate.png"></span>:</label> <select class="form-control" id="lpdate" name="lpdate" style="width:40px;padding:0;">'+
 						'<option value="01">01</option>'+
 						'<option value="02">02</option>'+
 						'<option value="03">03</option>'+
@@ -9,7 +9,7 @@ $( document ).ready(function() {
 	
 	var year = new Date().getFullYear();
 	
-	var elem_lpsem = '<div class="navbar-form"><label for="lpnum">'+loepoexp_lang.label_sem[l]+'<span class="glyphicon glyphicon-info-sign" data-image="löwenportal.png"></span>:</label> <select class="form-control" id="lpsem" name="lpsem" style="width:185px;">'+
+	var elem_lpsem = '<div class="navbar-form"><label for="lpsem">'+loepoexp_lang.label_sem[l]+'<span id="lpsem_support" class="glyphicon glyphicon-info-sign" data-image="lpsem.png"></span>:</label> <select class="form-control" id="lpsem" name="lpsem" style="width:185px;">'+
 						'<option value="'+(year-1)+'1">'+loepoexp_lang.label_ss[l]+' '+(year-1)+'</option>'+
 						'<option value="'+(year-1)+'2">'+loepoexp_lang.label_ws[l]+' '+(year-1)+'/'+year+'</option>'+
 						'<option value="'+year+'1" selected="selected">'+loepoexp_lang.label_ss[l]+' '+(year)+'</option>'+
@@ -18,7 +18,7 @@ $( document ).ready(function() {
 						'<option value="'+(year+1)+'2">'+loepoexp_lang.label_ws[l]+' '+(year+1)+'/'+(year+2)+'</option>'+
 						'</select></div>';
 	
-	var elem_lpnum = '<div class="navbar-form"><label for="lpnum">'+loepoexp_lang.label_prf[l]+'<span class="glyphicon glyphicon-info-sign" data-image="löwenportal.png"></span>:</label> <input class="form-control" type="text" id="lpnum" name="lpnum" style="width:75px;" /></div>';
+	var elem_lpnum = '<div class="navbar-form"><label for="lpnum">'+loepoexp_lang.label_prf[l]+'<span id="lpnum_support" class="glyphicon glyphicon-info-sign" data-image="lpnum.png"></span>:</label> <input class="form-control" type="text" id="lpnum" name="lpnum" style="width:75px;" /></div>';
 	
 	$('select#format').on('change', function(event){
 		
@@ -54,18 +54,48 @@ $( document ).ready(function() {
 		
 	});
 	
-	$(document).on("mouseenter", ".glyphicon.glyphicon-info-sign", function(){	
+	$(document).on("mouseenter", "#lpdate_support", function(){	
 		
         if ($(this).parent().children('div.image').length) {
             $(this).parent().children('div.image').show();
         } else {
             var image_name=$(this).data('image');
-            var imageTag='<div class="image" style="position:absolute;">'+'<img src="'+loepoexp_lang.path+image_name+'" alt="'+image_name+'" />'+'</div>';
+            var imageTag='<div class="image" style="position:absolute;">'+'<img src=".'+loepoexp_lang.path+image_name+'" alt="'+image_name+'" width="375px" />'+'</div>';
             $(this).parent().append(imageTag);
         }
 	});
 
-	$(document).on("mouseleave", ".glyphicon.glyphicon-info-sign", function(){
+	$(document).on("mouseleave", "#lpdate_support", function(){
+        $(this).parent().children('div.image').hide();
+    });
+
+    	$(document).on("mouseenter", "#lpsem_support", function(){	
+		
+        if ($(this).parent().children('div.image').length) {
+            $(this).parent().children('div.image').show();
+        } else {
+            var image_name=$(this).data('image');
+            var imageTag='<div class="image" style="position:absolute;">'+'<img src=".'+loepoexp_lang.path+image_name+'" alt="'+image_name+'" width="375px" />'+'</div>';
+            $(this).parent().append(imageTag);
+        }
+	});
+
+	$(document).on("mouseleave", "#lpsem_support", function(){
+        $(this).parent().children('div.image').hide();
+    });
+
+    	$(document).on("mouseenter", "#lpnum_support", function(){	
+		
+        if ($(this).parent().children('div.image').length) {
+            $(this).parent().children('div.image').show();
+        } else {
+            var image_name=$(this).data('image');
+            var imageTag='<div class="image" style="position:absolute;">'+'<img src=".'+loepoexp_lang.path+image_name+'" alt="'+image_name+'" width="375px" />'+'</div>';
+            $(this).parent().append(imageTag);
+        }
+	});
+
+	$(document).on("mouseleave", "#lpnum_support", function(){
         $(this).parent().children('div.image').hide();
     });
 
